3e1557279391b152df26f1a88f15eca05f5719d9
Dan <danrix89@gmail.com>
2016-05-06 11:44:34 -0600

    Trying the meal table...

--- Script1.js
+++ Script1.js
@@ -1,51 +1,57 @@
-// JavaScript source code
-angular.module('listDemo1', ['ngMaterial'])
-.config(function ($mdIconProvider) {
-    $mdIconProvider
-      .iconSet('communication', 'img/icons/sets/communication-icons.svg', 24);
-})
-.controller('AppCtrl', function ($scope) {
-    var imagePath = 'img/list/60.jpeg';
-    $scope.phones = [
-      { type: 'Home', number: '(555) 251-1234' },
-      { type: 'Cell', number: '(555) 786-9841' },
-      { type: 'Office', number: '(555) 314-1592' }
-    ];
-    $scope.todos = [
-      {
-          face: imagePath,
-          what: 'Brunch this weekend?',
-          who: 'Min Li Chan',
-          when: '3:08PM',
-          notes: " I'll be in your neighborhood doing errands"
-      },
-      {
-          face: imagePath,
-          what: 'Brunch this weekend?',
-          who: 'Min Li Chan',
-          when: '3:08PM',
-          notes: " I'll be in your neighborhood doing errands"
-      },
-      {
-          face: imagePath,
-          what: 'Brunch this weekend?',
-          who: 'Min Li Chan',
-          when: '3:08PM',
-          notes: " I'll be in your neighborhood doing errands"
-      },
-      {
-          face: imagePath,
-          what: 'Brunch this weekend?',
-          who: 'Min Li Chan',
-          when: '3:08PM',
-          notes: " I'll be in your neighborhood doing errands"
-      },
-      {
-          face: imagePath,
-          what: 'Brunch this weekend?',
-          who: 'Min Li Chan',
-          when: '3:08PM',
-          notes: " I'll be in your neighborhood doing errands"
-      },
-    ];
-});
+﻿var myList = [
+            { "Meal_name": "Chicken & Brocolli", "Ingredient": "chicken", "Type": "meat" },
+            { "Meal_name": "Chicken & Brocolli", "Ingredient": "rice", "Type": "grain" },
+            { "Meal_name": "Chicken & Brocolli", "Ingredient": "brocolli", "Type": "vegitable" }
+            ];
+
+// Builds the HTML Table out of myList json data from Ivy restful service.
+function buildHtmlTable() {
+    var columns = addAllColumnHeaders(myList);
+
+    for (var i = 0 ; i < myList.length ; i++) {
+        var row$ = $('<tr/>');
+        for (var colIndex = 0 ; colIndex < columns.length ; colIndex++) {
+            var cellValue = myList[i][columns[colIndex]];
+
+            if (cellValue == null) { cellValue = ""; }
+
+            row$.append($('<td/>').html(cellValue));
+        }
+        $("#meal_ingredients").append(row$);
+    }
+}
+
+// Adds a header row to the table and returns the set of columns.
+// Need to do union of keys from all records as some records may not contain
+// all records
+function addAllColumnHeaders(myList) {
+    //$("#meal_ingredients").append("<tr><th>Meal<th></tr>");
+    var columnSet = [];
+    var topHeaderTr$ = $('<tr/>');
+    var headerTr$ = $('<tr/>');
+    var topHeaderSet = false;
+
+    for (var i = 0 ; i < myList.length ; i++) {
+        var rowHash = myList[i];
+        for (var key in rowHash) {
+            if ($.inArray(key, columnSet) == -1) {
+                if (key == "Meal_name") {
+                    if (topHeaderSet == 0) {
+                        var columnCount = Object.keys(rowHash).length
+                        var headerTag = "<th colspan=\"" + columnCount + "\"/>"
+                        topHeaderTr$.append($(headerTag).html(rowHash[key]));
+                        topHeaderSet = true;
+                    }
+                }
+                else {
+                    columnSet.push(key);
+                    headerTr$.append($('<th/>').html(key));
+                }
+            }
+        }
+    }
+    $("#meal_ingredients").append(topHeaderTr$);
+    $("#meal_ingredients").append(headerTr$);
+
+    return columnSet;
+}
