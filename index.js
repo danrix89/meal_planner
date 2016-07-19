6c426bcfa722eca82a7db876b27858b4de44cda3
Dan <danrix89@gmail.com>
2016-07-18 22:00:14 -0600

    Testing Cookies

--- index.js
+++ index.js
@@ -77,37 +77,45 @@ $(document).ready(function () {
         modal.style.display = "none";
     }
 
-    function setCookie(cname, cvalue, exdays) {
+    function setCookie(cname, cvalue, exdays)
+    {
         var d = new Date();
         d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
         var expires = "expires=" + d.toGMTString();
         document.cookie = cname + "=" + cvalue + "; " + expires;
     }
 
-    function getCookie(cname) {
+    function getCookie(cname)
+    {
         var name = cname + "=";
         var ca = document.cookie.split(';');
-        for (var i = 0; i < ca.length; i++) {
+        for (var i = 0; i < ca.length; i++)
+        {
             var c = ca[i];
-            while (c.charAt(0) == ' ') {
+            while (c.charAt(0) == ' ')
+            {
                 c = c.substring(1);
             }
-            if (c.indexOf(name) == 0) {
+            if (c.indexOf(name) == 0)
+            {
                 return c.substring(name.length, c.length);
             }
         }
         return "";
     }
 
-    function checkCookie() {
-        var user = getCookie("username");
-        if (user != "") {
-            alert("Welcome again " + user);
-        } else {
-            user = prompt("Please enter your name:", "");
-            if (user != "" && user != null) {
-                setCookie("username", user, 30);
-            }
+    function checkCookie()
+    {
+        var is_already_welcomed = getCookie("is_already_welcomed");
+        if (is_already_welcomed != "")
+        {
+            is_to_welcome = false;
+        }
+        else
+        {
+            is_already_welcomed = "true";
+            setCookie("is_already_welcomed", is_already_welcomed, 30);
+            is_to_welcome = true;
         }
     }
 
