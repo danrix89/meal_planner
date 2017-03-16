6aff4d74fe1c7b7da548d3d621c5d67be5c3810c
Dan <danrix89@gmail.com>
2017-03-16 10:56:18 -0600

    Implementation for the following Trello card(s):
     - "Improve U.I. controls and styles" (https://trello.com/c/9VZz5qOn/21-improve-u-i-controls-and-styles)

--- public/meal_planner.js
+++ public/meal_planner.js
@@ -864,7 +864,7 @@ function drop_meal(event) {
         // Copy the image over with onclick functionality
         var source_meal_plan_image_element = document.getElementById(data);
         var target_meal_plan_image_element = event.target
-        var target_meal_plan_name_element = document.getElementById("calendar_day_data_container_name_element_" + target_meal_plan_image_element.dataset.mealId);
+        var source_meal_plan_name_element = document.getElementById("calendar_day_data_container_name_element_" + source_meal_plan_image_element.dataset.mealId);
         var target_day = event.target.parentElement.getAttribute("data-day");
         var source_day = source_meal_plan_image_element.parentElement.getAttribute("data-day");
         var image_path = source_meal_plan_image_element.getAttribute("data-image-path");
@@ -877,7 +877,7 @@ function drop_meal(event) {
         // Remove the image from the source parentElement
         source_meal_plan_image_element.parentElement.style.backgroundColor = "";
         source_meal_plan_image_element.parentElement.removeChild(source_meal_plan_image_element);
-        target_meal_plan_name_element.parentElement.removeChild(target_meal_plan_name_element);
+        source_meal_plan_name_element.parentElement.removeChild(target_meal_plan_name_element);
 
         // Update the "day" in the database record
         overwrite_meal_plan_day(target_day, source_day);
